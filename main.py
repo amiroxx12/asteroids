e1d971e37a44b13799aaad1661c123a265f83d61
l3m1r <amr@gmail.com>
2025-02-23 12:08:14 +0000

    update idk

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable,drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     dt = 0
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,13 +23,16 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        dt = clock.tick(60)/1000
-        player.update(dt) 
-        player.move(dt)    
+        dt = clock.tick(60) / 1000   
+        for sprite in updatable:
+            sprite.update(dt)
+            sprite.move(dt)
+        
         screen.fill((0, 0, 0))
-        player.draw(screen) 
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        player.update(dt)
+        pygame.display.flip()
 
     
 
