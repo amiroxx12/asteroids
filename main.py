c5a88310cf3d054db97863a3431044b9c933392c
l3m1r <amr@gmail.com>
2025-02-23 09:55:32 +0000

    shown triangle

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,10 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        dt = clock.tick(60)    
         screen.fill((0, 0, 0))
+        player.draw(screen) 
         pygame.display.flip()
-        dt = clock.tick(60)
-        print(f"Delta time: {dt/1000}")
 
     
 
