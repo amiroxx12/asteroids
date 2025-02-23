59d56fef539907889476c9b2d0ca201dd794d934
l3m1r <amr@gmail.com>
2025-02-23 09:29:10 +0000

    update1

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 2 > 0:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60)
+        print(f"Delta time: {dt/1000}")
+
+    
 
 if __name__ == "__main__":
     main()
